4a886bc12a394d534e11e0929545995c997a0ca7
Wellington Alves dos Santos <wellington.santos@clarifysistemas.com.br>
2019-05-28 16:31:40 -0300

    .

--- CargaRoteiro/Program.cs
+++ CargaRoteiro/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CargoRoteiro.DbContext;
 using Dapper;
 using System;
+using System.Data;
 using System.Linq;
 
 namespace CargaRoteiro
@@ -9,39 +10,58 @@ namespace CargaRoteiro
     {
         static void Main(string[] args)
         {
-            var ini = DateTime.Now;
-            var count = 0;
-            using (var conn = DbConnectionFactory.GetInstance())
+            int idx = 0;
+            IDbConnection conn = null;
+            try
             {
-                count = conn.QuerySingle<int>("select count(*) from PlanejadoVsRealizado");
-
-                while(true)
+                var ini = DateTime.Now;
+                //var count = 0;
+                using (conn = DbConnectionFactory.GetInstance())
                 {
-                    var lst = conn.Query<PlanejadoVsRealizado>(@"select top 500 Id=PlanejadoVsRealizadoId
+                    //count = conn.QuerySingle<int>("select count(*) from PlanejadoVsRealizado");
+                    
+                    while (true)
+                    {
+                        Console.Write("Obtendo registros");
+                        var lst = conn.Query<PlanejadoVsRealizado>(@"select top 10 Id=PlanejadoVsRealizadoId
                         from PlanejadoVsRealizadoRoteiro
                         group by PlanejadoVsRealizadoId
-                        having COUNT(*) <> MAX(OrdemRoteiro)").ToList();
-                    if (lst.Count == 0)
-                        break;
+                        having COUNT(*) <> MAX(OrdemRoteiro)", commandTimeout: int.MaxValue).ToList();
+                        Console.WriteLine(" - obtidos");
+                        if (lst.Count == 0)
+                            break;
 
-                    foreach (var item in lst)
-                    {
-                        var id = item.Id;
-                        conn.Execute(@"update PlanejadoVsRealizadoRoteiro
-                        set OrdemRoteiro = tab.OrdemRoteiro
-                        from
-                        (
-	                        select id, OrdemRoteiro = ROW_NUMBER() OVER(ORDER BY HorarioPlanejado, OrdemRoteiro)
-	                        from PlanejadoVsRealizadoRoteiro
-	                        where PlanejadoVsRealizadoId = @id
-                        ) tab
-                        where PlanejadoVsRealizadoRoteiro.Id = tab.Id", new { id });
-                    } 
+                        foreach (var item in lst)
+                        {
+                            Console.Write("{0}º fazendo", ++idx);
+                            var id = item.Id;
+                            conn.Execute(@"update PlanejadoVsRealizadoRoteiro
+                                            set OrdemRoteiro = tab.OrdemRoteiro
+                                            from
+                                            (
+	                                            select id, OrdemRoteiro = ROW_NUMBER() OVER(ORDER BY HorarioPlanejado, OrdemRoteiro)
+	                                            from PlanejadoVsRealizadoRoteiro
+	                                            where PlanejadoVsRealizadoId = @id
+                                            ) tab
+                                            where PlanejadoVsRealizadoRoteiro.Id = tab.Id", new { id });
+                            Console.WriteLine(" - feito");
+                        }
+                    }
                 }
+                var fim = DateTime.Now;
+                Console.WriteLine("{0} - {1} - {2}", ini, fim, fim.Subtract(ini));
             }
-            var fim = DateTime.Now;
-            Console.Write("{0} - {1} - {2}", ini, fim, fim.Subtract(ini));
-            Console.ReadKey();
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+                Console.ReadKey();
+            }
         }
     }
 }
